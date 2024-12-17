fae2a0a114fdc031d90b2d53e2974c722cab7e26
Anna Borisova <b0risova.ann4@yandex.ru>
2024-12-17 22:13:09 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+cook_book = {}
+with open('recepty.txt', encoding='utf-8') as file:
+    for line in file.read().split('\n\n'):
+        name, _, *args = line.split('\n')
+        cook_li = []
+        for arg in args:
+            ingredient_name, quantity,  measure  = arg.split(' | ')
+            cook_li.append({'ingredient_name': ingredient_name, 'quantity': quantity,'measure': measure})
+        cook_book = {name: cook_li}
+        print(cook_book)
+
+
