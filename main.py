d37e2875294f9cf2f8a01898f01e67fce3113e72
Anna Borisova <b0risova.ann4@yandex.ru>
2024-12-18 19:21:47 +0300

    True first commit

--- main.py
+++ main.py
@@ -7,7 +7,29 @@ with open('recepty.txt', encoding='utf-8') as file:
             ingredient_name, quantity,  measure  = arg.split(' | ')
             cook_li.append({'ingredient_name': ingredient_name, 'quantity': int(quantity),'measure': measure})
             cook_book[name] = cook_li
-print(cook_book)
+# print(cook_book)
+
+
+def get_shop_list_by_dishes(dishes, person_count ):
+    result = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for consist in cook_book[dish]:
+                if consist['ingredient_name'] in result:
+                    result[consist['ingredient_name']]['quantity'] += consist['quantity'] * person_count
+                else:
+                    result[consist['ingredient_name']] = {'measure': consist['measure'],'quantity': (consist['quantity'] * person_count)}
+        else:
+            print('Для данного блюда нет рецепта!')
+    return result
+
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2 ))
+
+
+
+
+
 
 
 
